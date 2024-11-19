46b31a3fba176ea14c5ec522bcf16ff1f4fef4d9
kwangil.kim <kwangil.kim@plantasset.com>
2024-11-19 17:30:51 +0900

    메뉴입히기

---
+++ NP.WPF/Local/Windows/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Jamesnet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.WPF.Local.Windows;
+public class MainWindowViewModel : ViewModelBase, IViewLoadable
+{
+	public MainWindowViewModel()
+	{
+
+	}
+
+	public void Loaded()
+	{
+
+	}
+}
---
+++ NP.WPF/Local/Windows/MonacoWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.WPF.Local.Windows;
+public class MonacoWindowViewModel
+{
+}
